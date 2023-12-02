43aa1266093e83fd4b8638fc2b7d309b81f57c97
julien <107534950+jntf@users.noreply.github.com>
2023-12-02 10:11:51 +0100

    Image Astro optimization

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="relative md:min-h-screen -mt-12">
         <div class="w-full mx-auto">
-            <img src="../assets/banner3.png" alt="banner" class="w-full h-[60vh] md:h-[90vh] object-cover">
+            <!-- <img src="../assets/banner3.png" alt="banner" class="w-full h-[60vh] md:h-[90vh] object-cover"> -->
+            <img :src="banner.src" alt="banner" class="w-full h-[60vh] md:h-[90vh] object-cover">
         </div>
 
         <div class="absolute top-20 left-5 md:w-1/3 h-[50vh] md:h-[90vh] flex flex-col items-center justify-center">
@@ -23,6 +24,10 @@
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
+
+const props = defineProps({
+    banner: Object,
+})
 
 interface WordEntry {
     text: string;
