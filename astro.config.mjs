3a384ff662979802bd2923517aa89a9dda34fbe5
julien <107534950+jntf@users.noreply.github.com>
2023-12-03 18:59:17 +0100

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind()]
+  site: 'https://26quatre.com',
+  integrations: [vue(), tailwind(), sitemap()]
 });
