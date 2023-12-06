3060a49b19ebbbd836c3c10352f101d57e395a11
julien <107534950+jntf@users.noreply.github.com>
2023-12-06 18:57:51 +0100

    m ga4 add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://26quatre.com',
-  integrations: [vue(), tailwind(), sitemap()]
+  integrations: [
+    vue(), 
+    tailwind(), 
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ]
 });
