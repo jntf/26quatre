f4ff17454d4f0de22acf09a48583b99d7f813dcb
julien <107534950+jntf@users.noreply.github.com>
2023-12-07 09:23:30 +0100

    ga4

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -24,7 +24,7 @@
                     </button>
                 </div>
                 <!-- Links -->
-                <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
+                <div class="hidden justify-end items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
                     <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                         <li v-for="link in links" :key="link.name" class="relative">
                             <a :href="'/' + link.url" class="px-3 py-2 flex items-center justify-center hover:text-yellow-400">
